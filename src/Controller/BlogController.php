46dc3373ab900b3ce653b8adf22d8b3bd52db687
Alla Dumenil <alla.dumenil@gmail.com>
2021-07-06 09:16:10 +0200

    dev enteté + html et css

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,23 +2,40 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Article;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class BlogController extends AbstractController
 {
+
+
     /**
+     *
+     * 
      * @Route("/blog", name="blog")
      */
-    public function index(): Response
+    public function blog(): Response
     {
-        return $this->render('blog/index.html.twig', [
+        // requete de selection BDD des articles
+
+        $repoArticles = $this->getDoctrine()->getRepository(Article::class);
+        dump($repoArticles);
+
+        $articles = $repoArticles->findAll();
+
+        dump($articles);
+
+
+        return $this->render('blog/blog.html.twig', [
             'controller_name' => 'BlogController',
         ]);
     }
 
     /**
+     *  Méthode permettant d'afficher l'ensemble de de la page d'accueil du blog
+     * 
      * @Route("/", name="home")
      */
     public function home(): Response
@@ -28,5 +45,16 @@ class BlogController extends AbstractController
             'age' => 25
         ]);
     }
+    /**
+     * Mèthode pour afficher les détailles d'un article
+     * 
+     * @Route("/blog/12", name="blog_show")
+     */ 
+    public function show() : Response
+    {
+        return $this->render('blog/show.html.twig');
+    }
+
+
 
 }
---
+++ src/DataFixtures/ArticleFixtures.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Article;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
+class ArticleFixtures extends Fixture
+{
+    // boucle va tourner 10 fois car nous avons besoin 10 articles 
+    // Pour pouvoir insérer des données dans la table SQL article, nous devons instancier son entité correspondante (Article),
+    // Symfony se sert l'objet entité $article pour injecter les valeurs dans les requetes SQL
+
+    public function load(ObjectManager $manager)
+    {
+        for($i = 1; $i <= 11; $i++)
+        {
+            $article = new Article;
+
+// On fait appel aux setteurs de l'objet entité afin de renseigner les titres, les contenu, 
+//les images et les dates des faux articles stockés en BDD:
+
+            $article->setTitre("Titre de l'article $i")
+                    ->setContenu("<p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Error dolores similique harum recusandae modi magni earum atque maxime quisquam. Sunt quasi dolore nobis illum. Ea nostrum unde iste at? Eligendi.</p>")
+                    ->setImage("https://pixabay.com/get/g122ec3928ce9fa40112c0b78beefc8396c281110e50c42a8a3076559133f42270e9afde9c987fca97667873887afb8ba_640.jpg")
+                    ->setDate(new \DateTime());
+// persist() recupere les donnée c'est comme "prepare" il prepare la requete SQL  d'insertion,
+//// persist() : méthode issue de la classe ObjectManager permettant de préaprer et de garder en méméoire les requetes d'insertion
+// $data=$bdd->prepare("INSERT INTO article VALUES ('getTitre()', 'getContenu()' etc..."));
+// Un manager (ObjectManager) en Symfony est un classe permettant, entre autre, 
+//de manipuler les lignes de la BDD (INSERT, UPDATE, DELETE)
+
+            $manager->persist($article);       
+        } 
+            // flush() : méthode issue de la classe ObjectManager permettant véritablement d'executer les requetes d'insertions en BDD
+
+        $manager->flush();
+    }
+
+}
